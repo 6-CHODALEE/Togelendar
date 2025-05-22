b62a4cd8bba574ec4549ca7b7b862868a2a3f364
조중석 <jungseok@jojungseog-ui-MacBookPro.local>
2025-05-22 16:54:03 +0900

    signup_login_logout 구현

---
+++ account/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# from django_resized import ResizedImageField
+
+# Create your models here.
+class User(AbstractUser):
+    address = models.CharField(max_length=255)
---
+++ account/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid() : 
+            form.save()
+            return redirect('account:login')
+
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('account:index')
+    else:
+        form = CustomAuthenticationForm()
+
+    context = {
+        'form':form,
+    }
+    return render(request, 'login.html', context)
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('account:index')
