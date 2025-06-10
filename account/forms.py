5465808bf342f21bf2be66c30d9fe254b28f88f4
조중석 <jungseok@jojungseog-ui-MacBookPro.local>
2025-06-10 16:52:19 +0900

    mypage 디자인 진행중

--- account/forms.py
+++ account/forms.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
-
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta():
+    class Meta:
         model = User
-        fields = ('username', 'password1', 'password2', 'email', 'profile_image', 'postcode', 'address')
+        fields = ['username', 'password1', 'password2', 'email', 'profile_image', 'postcode', 'address']
+        labels = {
+            'username': '아이디',
+            'email': '이메일',
+            'profile_image': '프로필 이미지',
+            'postcode': '우편번호',
+            'address': '주소',
+        }
 
         
 
